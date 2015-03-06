6d1ba694a857e3394641943d03af73b3f902a8be
flagarde <f.lagarde@ipnl.in2p3.fr>
2015-03-06 09:15:20 +0100

    Better texts and informations

--- source/src/Common/Geometry/Geometry.cc
+++ source/src/Common/Geometry/Geometry.cc
@@ -5,12 +5,11 @@ void Geometry::PrintGeom()
 { 
     for(int i=0;i<GetNumberPlates();++i)
     {
-    std::cout<<"Plans : "<<i<<"  "<<red<<"X:"<<GetPlatePositionX(i)<<"  Y:"<<GetPlatePositionY(i)<<"  Z:"<<GetPlatePositionZ(i)<<normal<<std::endl;
-    std::cout<<"Difs : "<<std::endl;
+    std::cout<<"Plane : "<<i+1<<"  "<<green<<"X : "<<GetPlatePositionX(i)<<"  Y : "<<GetPlatePositionY(i)<<"  Z : "<<GetPlatePositionZ(i)<<" Alpha : "<<GetDifPlateAlpha(i)<<" Beta : "<<GetDifPlateBeta(i)<<" Gamma : "<<GetDifPlateGamma(i)<<normal<<std::endl;
     for(std::map<int,Dif>::iterator it=Difs.begin();it!=Difs.end();++it)
     {
      if(GetDifNbrPlate((it->first))-1==i)
-     std::cout<<red<<"I:"<<(it->second).GetPositionX()<<"  J:"<<(it->second).GetPositionY()<<"  DifId:"<<(it->second).GetDifId()<<" DifType : "<<GetDifTypeName((it->second).GetDifId())<<normal<<std::endl;
+     std::cout<<green<<" DifId : "<<(it->second).GetDifId()<<" I : "<<(it->second).GetPositionX()<<"  J : "<<(it->second).GetPositionY()<<" DifType : "<<GetDifTypeName((it->second).GetDifId())<<normal<<std::endl;
     }
     }
    
