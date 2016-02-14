SDHCAL/EventBuilder

commit 36bb874582c72186e74182d57a1557cbcc905024

second step in removal of the Collections map in Analysis::processEvent

source/src/Analysis/Analysis.cc
@@ -915,10 +915,13 @@ void AnalysisProcessor::processEvent( LCEvent * evtP )
     else  std::cout<< red << "TRIGGER WITH SKIPED ..."<< normal <<std::endl;
     break;
     }*/
-  for(std::map<std::string,LCCollection*>::iterator ith=Collections.begin(); ith!=Collections.end(); ++ith)
+  for (std::vector<std::string>::iterator itcol=_hcalCollections.begin(); itcol!= _hcalCollections.end(); ++itcol)
     {
-      LCCollection* currentCollection=ith->second;
-      std::string currentCollectionName=ith->first;
+      std::string currentCollectionName=*itcol;
+      LCCollection* currentCollection=nullptr;
+      try { currentCollection= evtP ->getCollection(currentCollectionName); }
+      catch (DataNotAvailableException &e){ std::cout<<red<<currentCollectionName<< "not found"<<std::endl; break; }
+
       CellIDDecoder<CalorimeterHit> cd(currentCollection);
       int numElements = currentCollection->getNumberOfElements();
       for (int ihit=0; ihit < numElements; ++ihit)

commit 03a2a3b4fc2e8c61432e9bbe669a877ac2f09a57

fix bug introduced in commit 36bb874

source/src/Analysis/Analysis.cc
@@ -880,7 +880,7 @@ void AnalysisProcessor::processEvent( LCEvent * evtP )
       std::string currentCollectionName=*itcol;
       LCCollection* currentCollection=nullptr;
       try { currentCollection= evtP ->getCollection(currentCollectionName); }
-      catch (DataNotAvailableException &e){ std::cout<<red<<currentCollectionName<< "not found"<<std::endl; break; }
+      catch (DataNotAvailableException &e){ std::cout<<red<<currentCollectionName<< "not found"<<std::endl; continue; }
 
       Numbers[currentCollectionName]++;
       Progress(_skip,_GlobalEvents,_maxRecord,Numbers[currentCollectionName]);